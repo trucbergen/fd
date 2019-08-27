ad6f48d31495d6efd750b5dcc07bafc2f2423cfb
Travis CI User <travis@example.org>
2019-08-27 07:04:12 +0000

    Travis CI update 69 [ci skip]

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: fd")
-  packageStartupMessage("Version: 2019.08.19 at 09:55")
+  packageStartupMessage("Version: 2019.08.27 at 06:46")
 }
