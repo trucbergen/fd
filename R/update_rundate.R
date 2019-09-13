1a8972505af6c7e0d12450441c7299937f516efe
Richard White <w@rwhite.no>
2019-09-13 08:12:36 +0300

    rundate documentation

--- R/update_rundate.R
+++ R/update_rundate.R
@@ -1,5 +1,9 @@
 #' update_rundate
 #' Updates the rundate db tables
+#' @param package a
+#' @param package date_extraction a
+#' @param package date_results a
+#' @param package date_run a
 #' @export
 update_rundate <- function(package, date_extraction, date_results, date_run) {
   # date_extraction = date when the data file was extracted
